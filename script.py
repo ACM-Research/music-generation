70131c06d4c385ca32cb2a689b9e41c3afd0d57a
Daniel Angel <dka190000@utdallas.edu>
2021-03-08 22:03:40 -0600

    fixed the counting script and added some informational output

--- script.py
+++ script.py
@@ -2,16 +2,14 @@ from music21 import converter, instrument, note, chord
 import glob
 count = 0
 
-for file in glob.glob("/midi_files/*.mid"):
+for file in glob.glob("midi_files/*.mid"):
     midi = converter.parse(file)
     notes_to_parse = None
     parts = instrument.partitionByInstrument(midi)
-    print(parts.partName())
-    for instrument in parts:
-        if instrument.instrumentName() == "piano":
-            count+=1
+    for instr in parts:
+        if instr.partName == "Piano":
+            count += 1
+            # making sure the algorithm is counting
+            print(f"Found piano part #{count} in {file[11:]}")
 
-    #if parts.
-
-print(count)
-
+print(f"Total count: {count}")
