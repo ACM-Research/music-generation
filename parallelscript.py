0c03a2a3851a9ba6786210e1faa982c41cae2eb9
Daniel Angel <dka190000@utdallas.edu>
2021-03-09 16:12:32 -0600

    copied each track with piano into "piano_midis" folder using paralellscript.py

--- parallelscript.py
+++ parallelscript.py
@@ -1,23 +1,29 @@
 from music21 import converter, instrument, note, chord, common
 import glob
 import concurrent.futures
+import shutil
 
 count = 0
 cpus = 3
 
+
 def find(mfile):
     print("Checking", mfile)
-    
-    midi = converter.parse(mfile)
-    notes_to_parse = None
-    parts = instrument.partitionByInstrument(midi)
-    
+
+    try:
+        midi = converter.parse(mfile)
+        parts = instrument.partitionByInstrument(midi)
+    except(Exception):
+        print("Exception thrown at", mfile)
+        return(False, mfile)
+
     for instr in parts:
-        if instr.partName.lower().startswith("piano"): # avoid electric piano, etc
+        if instr.partName == "Piano":  # avoid electric piano, etc
             # making sure the algorithm is counting
             return (True, mfile)
-            
+
     return (False, mfile)
+
 
 all_files = glob.glob("midi_files/*.mid")
 
@@ -28,9 +34,8 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=cpus) as executor:
     for ret in executor.map(find, all_files):
         if ret[0]:
             count += 1
-            print(f"Found piano part in {ret[1][11:]} ({count} so far)") # #{count} 
-    
+            print(f"Found piano part in {ret[1][11:]} ({count} so far)")
+            shutil.copy(ret[1], "piano_midis")
+
 # with causes join
 print(f"Total count: {count}")
-
-
